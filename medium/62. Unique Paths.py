6c33508345624801372abf52f4b65bb66232080b
Evan Cui <evan@good-meowning.com>
2025-02-19 18:10:39 -0500

    46:48.26 O(mn) i think

---
+++ medium/62. Unique Paths.py
@@ -0,0 +1,9 @@
+from functools import cache
+class Solution:
+    @cache
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 0 or n == 0:
+            return 0
+        if m == 1 or n == 1:
+            return 1
+        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
