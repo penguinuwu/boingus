d0ecc8d701eba931e604e4455652e87830f06b8d
Evan Cui <evan@good-meowning.com>
2025-03-09 19:10:19 -0400

    41:21.90 O(n) check solution after 2mins

--- medium/128. Longest Consecutive Sequence.py
+++ medium/128. Longest Consecutive Sequence.py
@@ -1,19 +1,30 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # https://leetcode.com/problems/longest-consecutive-sequence/solutions/41057/simple-o-n-with-explanation-just-walk-each-streak
-        # cool solution but python set in has worst case O(n) ...
+        # https://leetcode.com/problems/longest-consecutive-sequence/solutions/41055/my-really-simple-java-o-n-solution-accepted/comments/191109/
+
         snums = set(nums)
         max_chain = 0
 
         for n in nums:
-            # only walk through streak forwards
-            if n - 1 not in snums:
-                # the +1 will ensure (curr - n) is at least 1
-                curr = n + 1
-                while curr in snums:
-                    # ... but if we remove curr, then n is decreased lmao
-                    snums.remove(curr)
-                    curr += 1
-                max_chain = max(max_chain, curr - n)
+            if n not in snums:
+                continue
+
+            left = n - 1
+            while left in snums:
+                snums.remove(left)
+                left -= 1
+
+            right = n + 1
+            while right in snums:
+                snums.remove(right)
+                right += 1
+
+            # n-1 and n+1 introduced an extra +2
+            # we need to keep +1 because number itself is consecutive 1
+            # so we do -1 to get rid of extra +1
+            max_chain = max(max_chain, right - left - 1)
+
+            if len(snums) == 0:
+                break
 
         return max_chain
