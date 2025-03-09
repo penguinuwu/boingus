cd91103a0b273f76818ff8c1e03d54f21dc7be10
Evan Cui <evan@good-meowning.com>
2025-03-09 17:17:35 -0400

    24:02.16 O(n) bucket sort lol

--- medium/347. Top K Frequent Elements.py
+++ medium/347. Top K Frequent Elements.py
@@ -1,12 +1,21 @@
-import heapq
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counts = defaultdict(lambda: 0)
         for n in nums:
             counts[n] += 1
 
-        order = [(-count, num) for num, count in counts.items()]
-        heapq.heapify(order)
+		# bucket sort
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for num, count in counts.items():
+            # no need to store a set because
+            # It is guaranteed that the answer is unique.
+            buckets[count].append(num)
 
-        return [heapq.heappop(order)[1] for _ in range(k)]
+        results = []
+        for i in range(len(nums), -1, -1):
+            if len(buckets[i]) > 0:
+                results.extend(buckets[i])
+                if len(results) >= k:
+                    break
+
+        return results
