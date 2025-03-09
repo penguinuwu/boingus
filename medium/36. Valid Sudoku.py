06f2850e2cc5dc27d3c0e68856a22152d303eaf5
Evan Cui <evan@good-meowning.com>
2025-03-09 18:24:04 -0400

    31:42.84 O(n^2) check solution

--- medium/36. Valid Sudoku.py
+++ medium/36. Valid Sudoku.py
@@ -1,21 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         for i in range(9):
-            # validate rows n columns
             row_nums = set()
             col_nums = set()
+            sub_box_nums = set()
             for j in range(9):
-                if not self.is_unique(board[i][j], row_nums) or not self.is_unique(board[j][i], col_nums):
-                    return False
+                si = ((i // 3) * 3) + (j // 3)
+                sj = ((i % 3) * 3) + (j % 3)
 
-        # validate sub-boxes
-        for i in range(3):
-            for j in range(3):
-                sub_box_nums = set()
-                for si in range(i*3, i*3 + 3):
-                    for sj in range(j*3, j*3 + 3):
-                        if not self.is_unique(board[si][sj], sub_box_nums):
-                            return False
+                if (not self.is_unique(board[i][j], row_nums) or \
+                    not self.is_unique(board[j][i], col_nums) or \
+                    not self.is_unique(board[si][sj], sub_box_nums)):
+                    return False
 
         return True
 
