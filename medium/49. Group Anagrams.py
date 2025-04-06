0add4deabac7a02e4bc8ada73cce1b5f7f6481a4
Evan Cui <evan@good-meowning.com>
2025-04-06 00:12:58 -0400

    5:42.11 O(n * s) no-sort i rember 😊

--- medium/49. Group Anagrams.py
+++ medium/49. Group Anagrams.py
@@ -1,7 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         grams = defaultdict(list)
+
         for word in strs:
-            key = tuple(sorted(list(word)))
+            key = self.hash_str(word)
             grams[key].append(word)
+
         return list(grams.values())
+
+    def hash_str(self, s: str):
+        # strs[i] consists of lowercase English letters
+        # so we count the characters
+        # thanks neetcode
+        counts = [0] * 26
+
+        for c in s:
+            counts[ord(c) - ord("a")] += 1
+
+        return tuple(counts)
