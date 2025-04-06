66ac7988681052ae42e423b04c5f0e2d41bc41a8
Evan Cui <evan@good-meowning.com>
2025-04-06 01:24:01 -0400

    30:08.48 O(n) sO(1) b-m voting alg is crazy

--- medium/229. Majority Element II.py
+++ medium/229. Majority Element II.py
@@ -1,7 +1,25 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count = defaultdict(lambda: 0)
+        n1, n2, c1, c2 = None, None, 0, 0
         for n in nums:
-            count[n] += 1
-        cutoff = len(nums) / 3
-        return [n for n, c in count.items() if c > cutoff]
+            if n == n1:
+                c1 += 1
+            elif n == n2:
+                c2 += 1
+            elif c1 == 0:
+                n1 = n
+                c1 = 1
+            elif c2 == 0:
+                n2 = n
+                c2 = 1
+            else:
+                c1 -= 1
+                c2 -= 1
+
+        results = []
+        if nums.count(n1) > len(nums) / 3:
+            results.append(n1)
+        if nums.count(n2) > len(nums) / 3:
+            results.append(n2)
+
+        return results
