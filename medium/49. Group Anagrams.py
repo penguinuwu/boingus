65c037b8e520d160fe1c28623225c097984f4105
Evan Cui <evan@good-meowning.com>
2025-04-06 00:11:25 -0400

    3:56:00 O(n s logs) i forgor no-sort solution 💀

--- medium/49. Group Anagrams.py
+++ medium/49. Group Anagrams.py
@@ -1,22 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
-
-        for s in strs:
-            h = self.str_to_hash(s)
-            anagrams[h].append(s)
-
-        return list(anagrams.values())
-
-
-    def str_to_hash(self, s):
-        # strs[i] consists of lowercase English letters
-        # so we count the characters
-        # thanks neetcode
-        count_chars = [0] * 26
-
-        for c in s:
-            count_index = ord(c) - ord("a")
-            count_chars[count_index] += 1
-
-        return tuple(count_chars)
+        grams = defaultdict(list)
+        for word in strs:
+            key = tuple(sorted(list(word)))
+            grams[key].append(word)
+        return list(grams.values())
