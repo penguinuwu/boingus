131111dd774b7d86c549d2b997affb4650f823f5
Evan Cui <evan@good-meowning.com>
2025-04-06 01:36:45 -0400

    41:05.31 O(n) sO(1) scalable b-m voting alg lmao

--- medium/229. Majority Element II.py
+++ medium/229. Majority Element II.py
@@ -1,25 +1,27 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n1, n2, c1, c2 = None, None, 0, 0
+        constraint = 3
+
+        n_c = [[None, 0] for _ in range(constraint - 1)]
+
         for n in nums:
-            if n == n1:
-                c1 += 1
-            elif n == n2:
-                c2 += 1
-            elif c1 == 0:
-                n1 = n
-                c1 = 1
-            elif c2 == 0:
-                n2 = n
-                c2 = 1
+            for i in range(constraint - 1):
+                if n == n_c[i][0]:
+                    n_c[i][1] += 1
+                    break
             else:
-                c1 -= 1
-                c2 -= 1
+                for i in range(constraint - 1):
+                    if n_c[i][1] == 0:
+                        n_c[i][0] = n
+                        n_c[i][1] = 1
+                        break
+                else:
+                    for i in range(constraint - 1):
+                        n_c[i][1] -= 1
 
         results = []
-        if nums.count(n1) > len(nums) / 3:
-            results.append(n1)
-        if nums.count(n2) > len(nums) / 3:
-            results.append(n2)
+        for i in range(constraint - 1):
+            if nums.count(n_c[i][0]) > len(nums) / constraint:
+                results.append(n_c[i][0])
 
         return results
