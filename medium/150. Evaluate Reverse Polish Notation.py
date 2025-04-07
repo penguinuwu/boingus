320e31d8130f9f73226bbbe03660367e3451bfdb
Evan Cui <evan@good-meowning.com>
2025-04-06 20:55:48 -0400

    22:14.26 O(n) sO(n) forgor pop(0) is linear oops

--- medium/150. Evaluate Reverse Polish Notation.py
+++ medium/150. Evaluate Reverse Polish Notation.py
@@ -1,21 +1,19 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        ops = set(("+", "-", "*", "/"))
         stack = []
 
-        while tokens:
-            curr = tokens.pop(0)
-
-            # this is "pythonic" hehe
-            try:
-                stack.append(int(curr))
-            except ValueError:
+        for curr in tokens:
+            if curr in ops:
                 self.evaluate(stack, curr)
+            else:
+                stack.append(int(curr))
 
         return stack[0]
     
     def evaluate(self, stack: List[int], operation: str):
-        n2 = int(stack.pop())
-        n1 = int(stack.pop())
+        n2 = stack.pop()
+        n1 = stack.pop()
 
         if operation == "+":
             stack.append(n1 + n2)
@@ -24,6 +22,6 @@ class Solution:
         elif operation == "*":
             stack.append(n1 * n2)
         elif operation == "/":
-            stack.append(math.trunc(n1 / n2))
+            stack.append(int(n1 / n2))
         else:
             raise RuntimeError()
