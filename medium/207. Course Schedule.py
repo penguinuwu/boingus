4173f1791c264271c210d7037a7735622785fb98
Evan Cui <evan@good-meowning.com>
2025-04-30 20:58:08 -0400

    simplify data structures

--- medium/207. Course Schedule.py
+++ medium/207. Course Schedule.py
@@ -1,37 +1,37 @@
 """
-24:22.59
+30:33.35
 O(v+e)
 sO(v+e)
 check solution after 3mins
+simplify data structures, sets are not required
 """
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # make adjacency list
-        adj_lst = defaultdict(lambda: {"required": set(), "required_by": set()})
+        # record number of incoming edges
+        prereq_count = [0] * numCourses
+
+        # make adjacency list of courses that depend on this
+        required_by = [[] for _ in range(numCourses)]
+
         for course, prereq in prerequisites:
-            adj_lst[course]["required"].add(prereq)
-            adj_lst[prereq]["required_by"].add(course)
+            required_by[prereq].append(course)
+            prereq_count[course] += 1
 
         # trim roots
-        roots = [c for c in range(numCourses) if len(adj_lst[c]["required"]) == 0]
+        available_courses = deque([c for c in range(numCourses) if prereq_count[c] == 0])
         taken_courses = 0
-
-        while roots:
-            new_roots = []
-
+        while available_courses:
             # take course
-            for course in roots:
-                taken_courses += 1
+            prereq = available_courses.popleft()
+            taken_courses += 1
 
-                # check all courses dependent on this course
-                for required_by in adj_lst[course]["required_by"]:
-                    adj_lst[required_by]["required"].remove(course)
+            # check all courses dependent on this course
+            for dependent in required_by[prereq]:
+                prereq_count[dependent] -= 1
 
-                    # check if course now has no dependencies
-                    if len(adj_lst[required_by]["required"]) == 0:
-                        new_roots.append(required_by)
-
-            roots = new_roots
+                # check if course now has no dependencies
+                if prereq_count[dependent] == 0:
+                    available_courses.append(dependent)
 
         return taken_courses == numCourses
