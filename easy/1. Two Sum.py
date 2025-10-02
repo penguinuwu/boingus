a2622a28a312bc432bfe55a92838e95e4c147ad7
Evan Cui <evan@good-meowning.com>
2025-10-01 20:54:29 -0400

    1:31.17 O(n^2) sO(1)

---
+++ easy/1. Two Sum.py
@@ -0,0 +1,18 @@
+"""
+1:31.17
+O(n^2)
+sO(1)
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
