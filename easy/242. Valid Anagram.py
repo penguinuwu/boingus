d90d4a6430fba531f78b3601c1a3f211edfaa75a
Evan Cui <evan@good-meowning.com>
2025-10-01 20:37:49 -0400

    3:12.36 O(n) sO(n) collections.Counter yay

--- easy/242. Valid Anagram.py
+++ easy/242. Valid Anagram.py
@@ -1,20 +1,13 @@
 """
-2:42.25
+3:12.36
 O(n)
 sO(n)
+i learned about collections.Counter yay
 """
+
+from collections import Counter
 
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        letters = defaultdict(lambda: 0)
-
-        for c in s:
-            letters[c] += 1
-        for c in t:
-            letters[c] -= 1
-
-        for counts in letters.values():
-            if counts != 0:
-                return False
-        return True
+        return Counter(s) == Counter(t)
