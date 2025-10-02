cd0b6a3ad3da6d80802f4873051dbbaf94c2548b
Evan Cui <evan@good-meowning.com>
2025-10-01 22:01:58 -0400

    15:05.50 O(n) sO(1) rember 1pass :]

--- medium/238. Product of Array Except Self.py
+++ medium/238. Product of Array Except Self.py
@@ -1,28 +1,30 @@
 """
-9:47.57
+15:05.50
 O(n)
 sO(1)
-forgot its 1passable
+1 pass beast
 """
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # carry over from 677bc32
+        # https://leetcode.com/problems/product-of-array-except-self/solutions/1342916/3-minute-read-mimicking-an-interview
+        # https://leetcode.com/problems/product-of-array-except-self/solutions/65627/o-n-time-and-o-1-space-c-solution-with-explanation
+
         n = len(nums)
+        prefix = 1
+        suffix = 1
         result = [1] * n
 
-        # left-to-right (nums[0] * ... * nums[i-1])
-        # skip first
-        left_product = 1
-        for i in range(1, n):
-            left_product *= nums[i - 1]
-            result[i] *= left_product
+        for i in range(n):
+            # left-to-right (nums[0] * ... * nums[i-1])
+            result[i] *= prefix
 
-        # right-to-left (nums[i+1] * ... * nums[-1])
-        # skip last
-        right_product = 1
-        for i in range(n - 1 - 1, -1, -1):
-            right_product *= nums[i + 1]
-            result[i] *= right_product
+            # right-to-left (nums[i+1] * ... * nums[-1])
+            result[n - 1 - i] *= suffix
+
+            prefix *= nums[i]
+            suffix *= nums[n - 1 - i]
 
         return result
