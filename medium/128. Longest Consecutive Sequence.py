00619b289e50aa12853bada7bea4d0045b59e907
Evan Cui <evan@good-meowning.com>
2025-10-02 19:37:29 -0400

    6:47.06 O(n) sO(n) clean up

--- medium/128. Longest Consecutive Sequence.py
+++ medium/128. Longest Consecutive Sequence.py
@@ -1,5 +1,5 @@
 """
-6:35.14
+6:47.06
 O(n)
 sO(n)
 i forgor check solutions lmao
@@ -13,20 +13,18 @@ class Solution:
 
         while nums_set:
             curr = nums_set.pop()
-            curr_longest = 1
 
             lower_bound = curr - 1
             while nums_set and lower_bound in nums_set:
                 nums_set.remove(lower_bound)
-                curr_longest += 1
                 lower_bound -= 1
 
             upper_bound = curr + 1
             while nums_set and upper_bound in nums_set:
                 nums_set.remove(upper_bound)
-                curr_longest += 1
                 upper_bound += 1
 
-            longest = max(curr_longest, longest)
+            # note: lower_bound and upper_bound are NOT in array, so need to -1
+            longest = max(longest, upper_bound - lower_bound - 1)
 
         return longest
