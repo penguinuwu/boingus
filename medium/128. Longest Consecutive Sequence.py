db166a9ec36c4e60bc2c49f9f81e9be4119702a2
Evan Cui <evan@good-meowning.com>
2025-10-02 19:35:01 -0400

    6:35.14 O(n) sO(n)

--- medium/128. Longest Consecutive Sequence.py
+++ medium/128. Longest Consecutive Sequence.py
@@ -1,30 +1,32 @@
+"""
+6:35.14
+O(n)
+sO(n)
+i forgor check solutions lmao
+"""
+
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # https://leetcode.com/problems/longest-consecutive-sequence/solutions/41055/my-really-simple-java-o-n-solution-accepted/comments/191109/
+        nums_set = set(nums)
+        longest = 0
 
-        snums = set(nums)
-        max_chain = 0
+        while nums_set:
+            curr = nums_set.pop()
+            curr_longest = 1
 
-        for n in nums:
-            if n not in snums:
-                continue
+            lower_bound = curr - 1
+            while nums_set and lower_bound in nums_set:
+                nums_set.remove(lower_bound)
+                curr_longest += 1
+                lower_bound -= 1
 
-            left = n - 1
-            while left in snums:
-                snums.remove(left)
-                left -= 1
+            upper_bound = curr + 1
+            while nums_set and upper_bound in nums_set:
+                nums_set.remove(upper_bound)
+                curr_longest += 1
+                upper_bound += 1
 
-            right = n + 1
-            while right in snums:
-                snums.remove(right)
-                right += 1
+            longest = max(curr_longest, longest)
 
-            # n-1 and n+1 introduced an extra +2
-            # we need to keep +1 because number itself is consecutive 1
-            # so we do -1 to get rid of extra +1
-            max_chain = max(max_chain, right - left - 1)
-
-            if len(snums) == 0:
-                break
-
-        return max_chain
+        return longest
