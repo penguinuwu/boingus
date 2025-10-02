ae69557c57697d14354f54fdbf891f93bd5bbd45
Evan Cui <evan@good-meowning.com>
2025-10-01 21:15:16 -0400

    8:37.35 O(nk) sO(nk)

--- easy/49. Group Anagrams.py
+++ easy/49. Group Anagrams.py
@@ -1,7 +1,7 @@
 """
-6:06.03
-O(n^2 log n)
-sO(n)
+8:37.35
+O(nk) where n = len(words), k = max len(word)
+sO(nk)
 """
 
 from collections import defaultdict
@@ -9,13 +9,17 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def sort_str(str) -> Tuple[str]:
-            return tuple(sorted(str))
+        def hash_str(word) -> Tuple[int]:
+            # 26 because "strs[i] consists of lowercase English letters"
+            key = [0] * 26
+            for c in word:
+                key[ord(c) - ord("a")] += 1
+            return tuple(key)
 
         groups = defaultdict(list)
 
         for word in strs:
-            key = sort_str(word)
+            key = hash_str(word)
             groups[key].append(word)
 
         return list(groups.values())
