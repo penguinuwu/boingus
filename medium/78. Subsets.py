4dfb1a2123de065910ab3f3f74b07b3679a7e7fc
Evan Cui <evan@good-meowning.com>
2025-10-04 01:02:14 -0400

    28:47.42 O(n * 2^n) sO(n * 2^n) forgot to record

--- medium/78. Subsets.py
+++ medium/78. Subsets.py
@@ -1,15 +1,15 @@
 """
-44:48.53
+28:47.42
 O(n * 2^n)
-sO(n) only counting recursion depth, not counting output array
+sO(n * 2^n)
 check solution after 18mins
 """
-
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         result = []
+
 
         def backtrack(start, curr_list) -> None:
             # add itself
@@ -19,10 +19,11 @@ class Solution:
             for i in range(start, n):
                 curr_list.append(nums[i])
 
-                # add i+1..n
-                backtrack(i + 1, curr_list)
+                # recurse with i+1..n
+                backtrack(i+1, curr_list)
 
                 curr_list.pop()
+
 
         backtrack(0, [])
         return result
