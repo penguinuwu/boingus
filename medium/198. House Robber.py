7bd6074a72047426559e0fb7276a74232208aa7e
Evan Cui <evan@good-meowning.com>
2025-10-05 14:53:18 -0400

    29:35.19 O(n) sO(1)

--- medium/198. House Robber.py
+++ medium/198. House Robber.py
@@ -1,26 +1,26 @@
 """
-13:57.30
+29:35.19
 O(n)
-sO(n) technically i modify input array so that counts as extra space use
+sO(1)
+check solution after 14mins to optimize sO(n) -> sO(1)
 """
 
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        max_amount = 0
 
-        for i in range(n):
-            house3 = i - 3
-            max_house3 = 0 if house3 < 0 else nums[house3]
+        max_house0 = nums[1] if 1 < n else 0
+        max_house1 = nums[0]
+        max_house2 = 0
 
-            house2 = i - 2
-            max_house2 = 0 if house2 < 0 else nums[house2]
+        for i in range(2, n):
+            curr_house = nums[i] + max(max_house1, max_house2)
+            max_house2 = max_house1
+            max_house1 = max_house0
+            max_house0 = curr_house
 
-            nums[i] += max(max_house2, max_house3)
-            max_amount = max(max_amount, nums[i])
-
-        return max_amount
+        return max(max_house0, max_house1)
 
 
 # [1,2,3,1]
