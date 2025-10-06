dc686a1fe5944822d57a2dd3b9c80a87079518d4
Evan Cui <evan@good-meowning.com>
2025-10-06 02:10:48 -0400

    Revert "32:50.46 O(s*n) sO(n)"
    This reverts commit e94a3070589ec7f0cc367ad4469dcdf22ca68ec3.

--- medium/322. Coin Change.py
+++ medium/322. Coin Change.py
@@ -1,29 +1,36 @@
 """
-32:50.46
-O(s*n) where s=len(coins), n=amount
+17:52.26
+O(s*n) without dp O(s^n) where s=len(coins), n=amount
 sO(n) dp
 check solution after 5mins
-bottom-up dp but it doesn't feel natural
+top-down dp + "dfs" + "backtrack"
 """
 
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coin_count_per_amount = [math.inf] * (amount + 1)
-        coin_count_per_amount[0] = 0
+        dp = {}
 
-        # order of coins don't matter
-        for c in coins:
-            # try each coin
-            for amt in range(c, amount + 1):
-                # try to use this coin for every valid amount
-                coin_count_per_amount[amt] = min(
-                    coin_count_per_amount[amt],
-                    coin_count_per_amount[amt - c] + 1,
-                )
+        def dfs_backtrack_dp(curr_amount):
+            if curr_amount in dp:
+                return dp[curr_amount]
 
-        return (
-            -1
-            if coin_count_per_amount[amount] == math.inf
-            else coin_count_per_amount[amount]
-        )
+            if curr_amount < 0:
+                dp[curr_amount] = math.inf
+                return dp[curr_amount]
+
+            if curr_amount == 0:
+                dp[curr_amount] = 0
+                return dp[curr_amount]
+
+            min_coins_needed = math.inf
+
+            for c in coins:
+                curr_coins_needed = dfs_backtrack_dp(curr_amount - c)
+                min_coins_needed = min(min_coins_needed, curr_coins_needed + 1)
+
+            dp[curr_amount] = min_coins_needed
+            return dp[curr_amount]
+
+        coins_needed = dfs_backtrack_dp(amount)
+        return -1 if coins_needed is math.inf else coins_needed
