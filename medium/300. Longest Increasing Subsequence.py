16e56bbdbb657218eb7132a69c79ad93142a6133
Evan Cui <evan@good-meowning.com>
2025-10-06 19:40:34 -0400

    26:55.38 O(n log n) sO(n)

--- medium/300. Longest Increasing Subsequence.py
+++ medium/300. Longest Increasing Subsequence.py
@@ -1,9 +1,10 @@
 """
-16:09.88
-O(n^2)
+26:55.38
+O(n log n)
 sO(n)
 preread solution
 be careful with duplicates (sequence[prev] **==** nums[curr])
+dp with binary search that took like 10mins to debug
 """
 
 
@@ -15,14 +16,16 @@ class Solution:
             if nums[curr] > sequence[-1]:
                 sequence.append(nums[curr])
             else:
-                for prev in range(len(sequence)):
-                    # check == because we cannot have duplicates in the sequence
-                    if sequence[prev] >= nums[curr]:
-                        # note: this sequence will NOT always be valid
-                        # but the length will always be the MAX VALID length
-                        # because we only append when n > seq[-1]
-                        # my explanation sucks but think about it and itll make sense
-                        sequence[prev] = nums[curr]
-                        break
+                l = 0
+                r = len(sequence) - 1
+                while l < r:
+                    m = (l + r) // 2
+                    if nums[curr] < sequence[m]:
+                        r = m
+                    elif nums[curr] > sequence[m]:
+                        l = m + 1
+                    else:
+                        l = r = m
+                sequence[l] = nums[curr]
 
         return len(sequence)
