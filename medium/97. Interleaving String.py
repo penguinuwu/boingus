b4b1a601a9d8139939fb4dc59c8c67ae92a40186
Evan Cui <evan@good-meowning.com>
2025-10-08 03:54:00 -0400

    22:15.59 O(n1 * n2) sO(n1 * n2)

--- medium/97. Interleaving String.py
+++ medium/97. Interleaving String.py
@@ -1,7 +1,8 @@
 """
-13:41.29
+22:15.59
 O(n1 * n2) n1=len(s1), n2=len(s2)
 sO(n1 * n2)
+clean up 2d memoization
 """
 
 
@@ -11,29 +12,33 @@ class Solution:
         n2 = len(s2)
         n3 = len(s3)
 
-        cache = {}
+        if n1 + n2 != n3:
+            return False
+        if s1 == s3 or s2 == s3:
+            return True
+
+        cache = [[None] * (n2 + 1) for _ in range(n1 + 1)]
 
         def dfs(idx1, idx2, idx3):
-            if (idx1, idx2, idx3) in cache:
-                return cache[(idx1, idx2, idx3)]
+            if not cache[idx1][idx2] is None:
+                return cache[idx1][idx2]
 
             if idx1 == n1 and idx2 == n2 and idx3 == n3:
-                cache[(idx1, idx2, idx3)] = True
+                cache[idx1][idx2] = True
                 return True
 
             if idx3 >= n3:
-                cache[(idx1, idx2, idx3)] = False
+                cache[idx1][idx2] = False
                 return False
 
-            interleaving = False
+            cache[idx1][idx2] = False
 
             if idx1 < n1 and s1[idx1] == s3[idx3]:
-                interleaving |= dfs(idx1 + 1, idx2, idx3 + 1)
+                cache[idx1][idx2] |= dfs(idx1 + 1, idx2, idx3 + 1)
 
             if idx2 < n2 and s2[idx2] == s3[idx3]:
-                interleaving |= dfs(idx1, idx2 + 1, idx3 + 1)
+                cache[idx1][idx2] |= dfs(idx1, idx2 + 1, idx3 + 1)
 
-            cache[(idx1, idx2, idx3)] = interleaving
-            return interleaving
+            return cache[idx1][idx2]
 
         return dfs(0, 0, 0)
