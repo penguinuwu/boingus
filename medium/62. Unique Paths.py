38082ebdd088170ceee2bcf98a4dd6a0230bb09c
Evan Cui <evan@good-meowning.com>
2025-10-08 01:41:52 -0400

    9:58.45 O(m*n) sO(m*n)

--- medium/62. Unique Paths.py
+++ medium/62. Unique Paths.py
@@ -1,9 +1,34 @@
-from functools import cache
+"""
+9:58.45
+O(m*n) without dp O(2^(m+n))
+sO(m*n)
+top-down dp
+took 2mins to debug `i+1 >= m` -> `i+1 < m` grr
+"""
+
+
 class Solution:
-    @cache
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == 0 or n == 0:
-            return 0
-        if m == 1 or n == 1:
-            return 1
-        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        # each cell represents number of unique paths from (i,j) to (m-1,n-1)
+        cache = [[-1] * n for _ in range(m)]
+        cache[m - 1][n - 1] = 1
+
+        def dfs(i, j):
+            if cache[i][j] != -1:
+                return cache[i][j]
+
+            # we can either go down or right
+            total = 0
+
+            # go down
+            if i + 1 < m:
+                total += dfs(i + 1, j)
+
+            # go right
+            if j + 1 < n:
+                total += dfs(i, j + 1)
+
+            cache[i][j] = total
+            return total
+
+        return dfs(0, 0)
